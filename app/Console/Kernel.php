398b5e02d186b955d24ed7587475d73067e249c7
edjacks1 <xlledjackllx@gmail.com>
2020-09-01 14:56:12 -0500

    cambios

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -7,20 +7,10 @@ use Laravel\Lumen\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
-    /**
-     * The Artisan commands provided by your application.
-     *
-     * @var array
-     */
+
     protected $commands = [
     ];
 
-    /**
-     * Define the application's command schedule.
-     *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
-     * @return void
-     */
     protected function schedule(Schedule $schedule)
     {
         $schedule->call('App\Http\Controllers\EventController@checkListNotification')->weekdays()
