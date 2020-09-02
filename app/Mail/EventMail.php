623b3c482964e63c3238a83687b533c7f8015bcb
edjacks1 <xlledjackllx@gmail.com>
2020-09-02 11:04:03 -0500

    asd

--- app/Mail/EventMail.php
+++ app/Mail/EventMail.php
@@ -12,21 +12,12 @@ class EventMail extends Mailable
     use Queueable, SerializesModels;
 
     public $details;
-    /**
-     * Create a new message instance.
-     *
-     * @return void
-     */
+
     public function __construct($details)
     {
         $this->details=$details;
     }
 
-    /**
-     * Build the message.
-     *
-     * @return $this
-     */
     public function build()
     {
         $subject="Información de tu Evento";
